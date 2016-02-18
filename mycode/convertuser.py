db4467846dcf281a67e2d5682ab46f27790cfd21
zsrichock <saza_thi@hotmail.com>
2016-02-18 17:22:40 +0700

    convert-v2

--- mycode/convertuser.py
+++ mycode/convertuser.py
@@ -17,8 +17,8 @@ class convuser():
                 if matchObj:
                     name = matchObj.group(1)
                     data = "W0nderl@nd" + lines.replace(',','')
-                    #self.thash[name]= self.topasswd(data)
-                    self.thash[name] =self.tomd5sum(self.topasswd(data))
+                    self.thash[name]= self.topasswd(data)
+                    #self.thash[name] =self.tomd5sum(self.topasswd(data))
                     '''
                     if name == 'xin31':
                         self.thash[name]= self.topasswd(data)
@@ -37,13 +37,27 @@ class convuser():
 
     def writefile(self, file):
         f =  open(file,'w')
-        f.write("<user-mapping>")
-        f.write("</user-mapping>")
+        f.write("<user-mapping>\n")
 
+        for  cn in  self.thash.keys():
+            f.write("\t<authorize username=\"%s\" password=\"%s\">\n" % (cn,self.thash[cn]))
+            #f.write("\t<authorize username=\"%s\" password=\"%s\" " %( cn, self.thash[cn]))
+            #f.write("encoding=\"md5\">\n")
+            f.write("\t\t<connection name=\"%s\">\n" %(cn))
+            f.write("\t\t\t<protocol>rdp</protocol>\n")
+            f.write("\t\t\t<param name=\"hostname\">110.34.250.200</param>\n")
+            f.write("\t\t\t<param name=\"username\">%s</param>\n" %(cn))
+            f.write("\t\t\t<param name=\"password\">%s</param>\n" %(self.thash[cn]))
+            f.write("\t\t\t<param name=\"color-depth\">16</param>\n")
+            f.write("\t\t</connection>\n")
+            f.write("\t</authorize>\n")
+
+        f.write("</user-mapping>\n")
         f.closed
+
 
 if __name__ == "__main__":
     a = convuser()
     a.readfile('./listIpmi')
-    a.showthash()
+    #a.showthash()
     a.writefile('./user-mapping.xml')

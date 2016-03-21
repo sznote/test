011c8560e96219c1aefa35a80501e789db3eaea7
zsrichock <saza_thi@hotmail.com>
2016-03-21 21:34:40 +0700

    master

--- mycode/ftpupload.py
+++ mycode/ftpupload.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 
+import urllib2
 import ftplib
 import os
+
+
+
 
 server = '110.34.250.200'
 username = 'dbipmi'
 userpass = 'HXsY0KnB1'
 file = 'listIpmi'
+url = 'http://portal.vpls.net/api/listIpmi'
+
+response = urllib2.urlopen(url)
+fh = open(file,"w")
+fh.write(response.read())
+fh.close()
+
 ftp_conn =  ftplib.FTP(server,username,userpass)
 fh = open(file,'rb')
 ftp_conn.storbinary('STOR listIpmi',fh)
